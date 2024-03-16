7f47d25438c9d2d218323b862367f8408d44bb84
HuangRui <hr837@163.com>
2024-03-16 21:35:13 +0800

    feat: 增加Vue,Element-UI

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>💖 Hello Duang!</h1>
+    <!-- custom component-->
+    <Conunter/>
+</template>
+
+<script setup lang="ts">
+ import Conunter from "./components/Conunter.vue";
+</script>
---
+++ src/components/Conunter.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="app-counter">
+        <el-button @click="onIncrease" icon="plus">增加</el-button>
+        <div class="app-counter_text">Count is : {{ counter }}</div>
+        <el-button @click="onDecrease" icon="minus">减少</el-button>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const counter = ref(1);
+
+const onIncrease = () => counter.value += 1;
+const onDecrease = () => counter.value -= 1;
+
+</script>
+
+<style>
+.app-counter {
+    display: flex;
+    align-items: center;
+}
+
+.app-counter_text {
+    min-width: 120px;
+    text-align: center;
+}
+</style>
